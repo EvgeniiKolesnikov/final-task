5386886e9999388349222388d801139fd6d96545
Evgenii Kolesnikov <jekow@yandex.ru>
2023-02-09 09:46:12 +0300

    feat: update files

--- src/Games/GameSpeedMatch/SpeedMatch.tsx
+++ src/Games/GameSpeedMatch/SpeedMatch.tsx
@@ -1,15 +1,14 @@
-import { is } from 'immer/dist/internal';
-import React, { useState, useEffect, useRef } from 'react';
+import succesSoundPath from '../../assets/sounds/success.mp3';
+import failureSoundPath from '../../assets/sounds/failure.mp3';
+import pic from '../../assets/logo192.png';
+import React, { useState, useEffect, useRef, ReactElement } from 'react';
 import Controls from '../../components/Controls/Controls';
 import StartGame from '../../components/StartGame/StartGame';
 import Cards from './components/Cards/Cards';
 import GameStats from '../../components/GameStats/GameStats';
 import './SpeedMatch.scss';
-import im from '../../assets/success.mp3';
-import im1 from '../../assets/rss.svg';
-import im2 from '../../assets/puzzle.jpg';
-import im3 from '../../assets/puzzle1.JPG';
-import im4 from '../../assets/Travel-Mobile.png';
+import AnswerIndicator from '../../components/AnswerIndicator/AnswerIndicator';
+import Results from '../../components/Results/Results';
 
 const gameDescription =
   'In Speed Match you only need to determine if the symbols are the same.';
@@ -19,38 +18,185 @@ const getNextCard = () => {
   return figures[Math.floor(Math.random() * figures.length)];
 };
 
-const t = new Audio(im);
+const succesSound = new Audio(succesSoundPath);
+const failureSound = new Audio(failureSoundPath);
+
 export default function SpeedMatch() {
   const [isStarted, setIsStarted] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(true);
+  const [isAnswerGetted, setIsAnswerGetted] = useState(false);
   const [currentCard, setCurrentCard] = useState('rectangle');
+  const [streak, setStreak] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+  const [score, setScore] = useState(0);
+  const [timer, setTimer] = useState(45);
+  const multiplierTemp = useRef(1);
   const prevCard = useRef('');
+  const [isGameEnd, setIsGameEnd] = useState(false);
+  const [answersCount, setAnswersCount] = useState(0);
+  const [rightAnswersCount, setRightAnswersCount] = useState(0);
+
+  const changeMultiplayer = (isRightAnswer: boolean, streak: number) => {
+    if (isRightAnswer && streak == 4) {
+      setMultiplier(prev => {
+        multiplierTemp.current = prev + 1;
+        return multiplierTemp.current;
+      });
+      setStreak(0);
+    }
+    if (!isRightAnswer && streak == 0) {
+      setMultiplier(prev => {
+        return prev === 1 ? prev : prev - 1;
+      });
+    }
+  };
+
+  const changeScore = (isSuccess: boolean) => {
+    if (isSuccess) {
+      setRightAnswersCount(prev => prev + 1);
+      setScore(prev => prev + 50 * multiplierTemp.current);
+      setStreak(prev => {
+        changeMultiplayer(true, prev);
+        return prev + 1;
+      });
+      return;
+    }
+    setStreak(prev => {
+      changeMultiplayer(false, prev);
+      return 0;
+    });
+  };
+
+  const handleAnswer = async (isRightAnswer: boolean) => {
+    changeScore(isRightAnswer);
+    setIsSuccess(isRightAnswer);
+    if (isRightAnswer) {
+      succesSound.pause();
+      succesSound.currentTime = 0;
+      await succesSound.play();
+    } else {
+      failureSound.pause();
+      failureSound.currentTime = 0;
+      await failureSound.play();
+    }
+  };
+
+  const chekIsRightAnswer = (
+    event: KeyboardEvent,
+    current: string,
+    prev: string
+  ) => {
+    setAnswersCount(prev => prev + 1);
+    if (current === prev && event.key === 'ArrowRight') {
+      void handleAnswer(true);
+    }
+    if (current !== prev && event.key === 'ArrowLeft') {
+      void handleAnswer(true);
+    }
+    if (current === prev && event.key === 'ArrowLeft') {
+      void handleAnswer(false);
+    }
+    if (current !== prev && event.key === 'ArrowRight') {
+      void handleAnswer(false);
+    }
+  };
+
+  const onControlsHandler = (event: KeyboardEvent) => {
+    if (
+      event.key === 'ArrowRight' ||
+      (event.key === 'ArrowLeft' && !isGameEnd)
+    ) {
+      document
+        .querySelector('.cards__field-previous')
+        ?.classList.add('cards__field-previous_used');
+      setCurrentCard(currentCard => {
+        setIsAnswerGetted(true);
+        chekIsRightAnswer(event, currentCard, prevCard.current);
+        prevCard.current = currentCard;
+        const newCurrent = getNextCard();
+        return newCurrent;
+      });
+    }
+  };
+
+  const startTimer = () => {
+    const interval = setInterval(() => {
+      setTimer(prevTimer => {
+        if (prevTimer === 0) {
+          clearInterval(interval);
+          setIsGameEnd(true);
+          setIsStarted(false);
+          return 0;
+        } else {
+          return prevTimer - 1;
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  };
 
   const onPlayHandler = () => {
     setIsStarted(true);
+    startTimer();
   };
+
+  const onRetryHandler = () => {
+    setIsGameEnd(false);
+    setIsStarted(true);
+    setScore(0);
+    setMultiplier(1);
+    setStreak(0);
+    setAnswersCount(0);
+    setRightAnswersCount(0);
+    setTimer(45);
+    multiplierTemp.current = 1;
+    setIsAnswerGetted(false);
+    startTimer();
+    setCurrentCard('rectangle');
+    document
+      .querySelector('.cards__field-previous')
+      ?.classList.toggle('cards__field-previous_used');
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', onControlsHandler);
+  }, []);
 
   return (
     <div className="speed-match">
-      {!isStarted && (
+      {!isStarted && !isGameEnd && (
         <StartGame
           title="SpeedMatch"
           description={gameDescription}
           onPlayHandler={onPlayHandler}
         />
       )}
-      <source media="(min-width: )" />
-      <img src={im1} alt="" />
-      <img src="" alt="" />
+      {/* <img src={pic} alt="sdf" /> */}
       {isStarted && (
         <>
-          <GameStats />
+          <GameStats
+            score={score}
+            streak={streak}
+            multiplier={multiplier}
+            timer={timer}
+          />
           <h2 className="speed-match__title">
             Does the CURRENT card match the card that came IMMEDIATELY BEFORE
             it?
           </h2>
           <Cards currentCard={currentCard} />
+          {isAnswerGetted && <AnswerIndicator isSuccess={isSuccess} />}
           <Controls />
         </>
+      )}
+      {isGameEnd && (
+        <Results
+          score={score}
+          correct={rightAnswersCount}
+          count={answersCount}
+          onRetryHandler={onRetryHandler}
+        />
       )}
     </div>
   );
